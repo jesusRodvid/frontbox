e98349385b4c173f57bbb749d409374b4f25b746
JESUS DANIEL RODRIGUEZ VIDAL <jdaniel.rodriguez@ikeasi.com>
2022-05-09 13:30:38 +0100

    added some of the routing an the navbar

--- src/routers/DashboardRoutes.js
+++ src/routers/DashboardRoutes.js
@@ -3,21 +3,20 @@ import { Navbar } from '../components/nav/Navbar'
 import { Routes, Route } from 'react-router-dom'
 import { ItemsScreen } from '../components/items/ItemsScreen'
 import { SupplierScreen } from '../components/suppliers/SupplierScreen'
-import { SearchScreen } from '../search/SearchScreen'
+import { SearchScreen } from "../search/SearchScreen";
 
 export const DashboardRoutes = () => {
-    return (
-        <>
-          <Navbar />
-            <Routes>
-                <Route path = "items" element = {<ItemsScreen />} />
-                <Route path = "suppliers" element = {<SupplierScreen />} />
-                <Route path = "search" element = {<SearchScreen />} />
-                <Route path = "/" element = {<ItemsScreen />} />
-
-
-                
-            </Routes>  
-        </>
-    )
-}
+  return (
+    <>
+      <Navbar />
+      <div>
+        <Routes>
+          <Route path="items" element={<ItemsScreen />} />
+          <Route path="suppliers" element={<SupplierScreen />} />
+          <Route path="search" element={<SearchScreen />} />
+          <Route path="/" element={<ItemsScreen />} />
+        </Routes>
+      </div>
+    </>
+  );
+};
